ebff003d9d801559869c908c0bb7df7a96207c1c
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-07-27 03:05:31 -0500

    Added Lucid icons

--- src/components/ActionBar.jsx
+++ src/components/ActionBar.jsx
@@ -1,82 +1,225 @@
+import {
+  Bold,
+  Code2,
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+  Italic,
+  Link,
+  List,
+  ListChecks,
+  ListOrdered,
+  Quote,
+  Strikethrough,
+  Underline,
+} from 'lucide-react'
 import React from 'react'
 
-const wrapSelectionWith = ({value, setValue, mdInput}, open, close=null) => {
+const wrapSelectionWith = (
+  { value, setValue, mdInput },
+  open,
+  close = null,
+) => {
   const cursor = {
     start: mdInput.current.selectionStart,
     end: mdInput.current.selectionEnd,
   }
 
-  let midSection = " "
+  let midSection = ' '
   if (cursor.start != cursor.end) {
     midSection = value.substring(cursor.start, cursor.end)
   }
 
-  setValue(value.substring(0, cursor.start) + `${open}${midSection}${close? close : open}` + value.substring(cursor.end))
-  
+  setValue(
+    value.substring(0, cursor.start) +
+      `${open}${midSection}${close ? close : open}` +
+      value.substring(cursor.end),
+  )
+
   setTimeout(() => {
-    mdInput.current.focus();
-    mdInput.current.setSelectionRange(cursor.start + open.length, cursor.end + open.length);
-  }, 0);
+    mdInput.current.focus()
+    mdInput.current.setSelectionRange(
+      cursor.start + open.length,
+      cursor.end + open.length,
+    )
+  }, 0)
 }
 
-const addToStartOfLine = ({value, setValue, mdInput}, symbol) => {
-  const cursorStart = mdInput.current.selectionStart;
-  const SOL = findStartOfLine(value, cursorStart);
+const addToStartOfLine = ({ value, setValue, mdInput }, symbol) => {
+  const cursorStart = mdInput.current.selectionStart
+  const SOL = findStartOfLine(value, cursorStart)
 
   setValue(value.substring(0, SOL) + symbol + value.substring(SOL))
 }
 
-const doHyperLink = ({value, setValue, mdInput}) => {
-  const endCursor = mdInput.current.selectionEnd;
-  wrapSelectionWith({value, setValue, mdInput}, "[", "](url)")
+const doHyperLink = ({ value, setValue, mdInput }) => {
+  const endCursor = mdInput.current.selectionEnd
+  wrapSelectionWith({ value, setValue, mdInput }, '[', '](url)')
 
   setTimeout(() => {
-    mdInput.current.focus();
-    mdInput.current.setSelectionRange(endCursor + 3, endCursor + 6);
-  }, 0);
+    mdInput.current.focus()
+    mdInput.current.setSelectionRange(endCursor + 3, endCursor + 6)
+  }, 0)
 }
 
 const findStartOfLine = (string, position) => {
-  let startOfLine = 0;
+  let startOfLine = 0
 
   for (let i = position; i >= 0; i--) {
     if (string[i] == '\n') {
-      startOfLine = i + 1;
-      break;
+      startOfLine = i + 1
+      break
     }
   }
 
-  return startOfLine;
+  return startOfLine
 }
 
 export const ActionBar = (props) => {
+  const iconSize = 20 // px
+  const iconStroke = 1.5 // px
   return (
-    <span className='action-bar'>
-        <button className='icon-button' onClick={() => {wrapSelectionWith(props, "**")}}><img className="icon-button__img" src='/icons/formatting/bold.svg'></img></button>
-        <button className='icon-button' onClick={() => {wrapSelectionWith(props, "*")}}><img className="icon-button__img" src='/icons/formatting/italic.svg'></img></button>
-        <button className='icon-button' onClick={() => {wrapSelectionWith(props, "<u>", "</u>")}}><img className="icon-button__img" src='/icons/formatting/underline.svg'></img></button>
-        <button className='icon-button' onClick={() => {wrapSelectionWith(props, "~")}}><img className="icon-button__img" src='/icons/formatting/strike-through.svg'></img></button>
-        
-        <span className='divider'></span>
+    <span className="action-bar">
+      <button
+        className="icon-button"
+        onClick={() => {
+          wrapSelectionWith(props, '**')
+        }}
+      >
+        <Bold size={iconSize} strokeWidth={iconStroke}></Bold>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          wrapSelectionWith(props, '*')
+        }}
+      >
+        <Italic size={iconSize} strokeWidth={iconStroke}></Italic>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          wrapSelectionWith(props, '<u>', '</u>')
+        }}
+      >
+        <Underline size={iconSize} strokeWidth={iconStroke}></Underline>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          wrapSelectionWith(props, '~')
+        }}
+      >
+        <Strikethrough size={iconSize} strokeWidth={iconStroke}></Strikethrough>
+      </button>
 
-        <button className='icon-button' onClick={() => {doHyperLink(props)}}><img className="icon-button__img" src='/icons/formatting/link.svg'></img></button>
-        <button className='icon-button' onClick={() => {wrapSelectionWith(props, "``")}}><img className="icon-button__img" src='/icons/formatting/code.svg'></img></button>
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, " > ")}}><img className="icon-button__img" src='/icons/formatting/quote.svg'></img></button>
-        
-        <span className='divider'></span>
+      <span className="divider"></span>
 
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, "1. ")}}><img className="icon-button__img" src='/icons/formatting/ordered-list.svg'></img></button>
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, " - ")}}><img className="icon-button__img" src='/icons/formatting/un-ordered-list.svg'></img></button>
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, " - [ ] ")}}><img className="icon-button__img" src='/icons/formatting/todo.svg'></img></button>
-        
-        <span className='divider'></span>
-        
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, "# ")}}><img className="icon-button__img" src='/icons/formatting/h1.svg'></img></button>
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, "## ")}}><img className="icon-button__img" src='/icons/formatting/h2.svg'></img></button>
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, "### ")}}><img className="icon-button__img" src='/icons/formatting/h3.svg'></img></button>
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, "#### ")}}><img className="icon-button__img" src='/icons/formatting/h4.svg'></img></button>
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, "##### ")}}><img className="icon-button__img" src='/icons/formatting/h5.svg'></img></button>
-        <button className='icon-button' onClick={() => {addToStartOfLine(props, "###### ")}}><img className="icon-button__img" src='/icons/formatting/h6.svg'></img></button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          doHyperLink(props)
+        }}
+      >
+        <Link size={iconSize} strokeWidth={iconStroke}></Link>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          wrapSelectionWith(props, '``')
+        }}
+      >
+        <Code2 size={iconSize} strokeWidth={iconStroke}></Code2>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, ' > ')
+        }}
+      >
+        <Quote size={iconSize} strokeWidth={iconStroke}></Quote>
+      </button>
+
+      <span className="divider"></span>
+
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, '1. ')
+        }}
+      >
+        <ListOrdered size={iconSize} strokeWidth={iconStroke}></ListOrdered>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, ' - ')
+        }}
+      >
+        <List size={iconSize} strokeWidth={iconStroke}></List>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, ' - [ ] ')
+        }}
+      >
+        <ListChecks size={iconSize} strokeWidth={iconStroke}></ListChecks>
+      </button>
+
+      <span className="divider"></span>
+
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, '# ')
+        }}
+      >
+        <Heading1 size={iconSize} strokeWidth={iconStroke}></Heading1>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, '## ')
+        }}
+      >
+        <Heading2 size={iconSize} strokeWidth={iconStroke}></Heading2>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, '### ')
+        }}
+      >
+        <Heading3 size={iconSize} strokeWidth={iconStroke}></Heading3>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, '#### ')
+        }}
+      >
+        <Heading4 size={iconSize} strokeWidth={iconStroke}></Heading4>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, '##### ')
+        }}
+      >
+        <Heading5 size={iconSize} strokeWidth={iconStroke}></Heading5>
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          addToStartOfLine(props, '###### ')
+        }}
+      >
+        <Heading6 size={iconSize} strokeWidth={iconStroke}></Heading6>
+      </button>
     </span>
   )
 }

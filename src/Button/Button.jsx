9321a6c09a054fb8f7e23d01bb0fecd94b731c11
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-08-15 13:08:33 -0500

    1 : restyled button components

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import Icon from '../Icon/Icon'
+import { Icon } from '../Icon'
 
 import './Button.css'
 
-export const Button = ({children, onClick, variant, leftIcon=null, rightIcon=null}) => {
+export const Button = ({children, onClick, variant = "filled", actionType = "progressive", leftIcon=null, rightIcon=null}) => {
+  let buttonClass = `button button--${variant} button--${actionType}`;
+  
   return (
-    <button className={`button button--${variant}`} onClick={onClick}>
-      {leftIcon != null && <Icon size={18} strokeWidth={2} name={leftIcon} />}
+    <button className={buttonClass} onClick={onClick}>
+      {leftIcon != null && <Icon size={20} strokeWidth={1.5} name={leftIcon} />}
       {children}
-      {rightIcon != null && <Icon size={18} strokeWidth={2} name={rightIcon} />}
+      {rightIcon != null && <Icon size={20} strokeWidth={1.5} name={rightIcon} />}
     </button>
   )
 }

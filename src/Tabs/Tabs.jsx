df98c6b9aec893e736b7259200da09709a82327a
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-08-18 09:57:39 -0500

    6 : Added icon-only tabs

--- src/Tabs/Tabs.jsx
+++ src/Tabs/Tabs.jsx
@@ -1,30 +1,31 @@
 import React, { useState } from 'react'
 import classNames from 'classnames'
+import { Icon } from '../Icon'
 
 import './Tabs.css'
 
-export function TabGroup({ children, variant="filled" }) {
+export function TabGroup({ children, filled = true }) {
   const [activeTab, setActiveTab] = useState(0)
 
   return (
     <div className="tab-group"> 
       <div className="tab-group__tab-list">
         {
-          children.map((tab, index) => {
-            let tabClass = classNames(
-              'tab',
-              `tab--${variant}`,
-              {
-                'tab--active': activeTab == index,
-              }
-            )
-            
-            return (
-              <button key={index} className={tabClass} onClick={() => setActiveTab(index)}>
-                {tab.props.label}
-              </button>
-            )
-          })
+          children.map((tab, index) => 
+            <TabButton 
+              key={index}
+
+              leftIcon = {tab.props.leftIcon}
+              label = {tab.props.label}
+              
+              index = {index}
+              setActiveTab = {setActiveTab}
+              activeTab = {activeTab}
+              
+              filled = {tab.props.filled}
+              disabled = {tab.props.disabled}
+            ></TabButton>
+          )
         }
       </div>
 
@@ -35,4 +36,34 @@ export function TabGroup({ children, variant="filled" }) {
 
 export function Tab({ children }) {
   return <section className="tab-group__panel">{children}</section>
+}
+
+const TabButton = ({leftIcon, index, setActiveTab, activeTab, filled = true, disabled = false, label = null, padding = "md"}) => {
+  const [isHovering, setIsHovering] = useState(false)
+
+  let tabClass = classNames(
+    'tab',
+    `padding-${padding}`,
+    {
+      'tab--filled': !disabled && filled,
+      'tab--icon-only': label === null,
+      'tab--active': !disabled && activeTab === index,
+      'tab--hover': !disabled && isHovering,
+      'tab--disabled': disabled
+    }
+  )
+  
+  return (
+    <button 
+      className={tabClass} 
+      onClick={() => setActiveTab(index)} 
+      disabled={disabled}
+
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)} 
+    >
+      {leftIcon != null && <Icon size={20} strokeWidth={1.5} name={leftIcon} />}
+      {label}
+    </button>
+  )
 }

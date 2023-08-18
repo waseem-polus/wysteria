df69f6997dbaa7d2e3d76ec2e9e842fd8ae0bf53
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-08-17 22:39:15 -0500

    1 : Added icon-only button

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { Icon } from '../Icon'
+import classNames from 'classnames';
 
 import './Button.css'
 
-export const Button = ({children, onClick, variant = "filled", actionType = "progressive", leftIcon=null, rightIcon=null}) => {
-  let buttonClass = `button button--${variant} button--${actionType}`;
-  
+export const Button = ({
+  onClick, 
+  children = null, 
+  padding = "md",
+  variant = "filled", 
+  actionType = "progressive", 
+  leftIcon=null, 
+  rightIcon=null
+}) => {
+  let buttonClass = classNames(
+    `button button--${variant}`,
+    `button--${actionType}`,
+    `padding-${padding}`,
+    { 'button--icon-only': children === null }
+  )
+
   return (
     <button className={buttonClass} onClick={onClick}>
       {leftIcon != null && <Icon size={20} strokeWidth={1.5} name={leftIcon} />}

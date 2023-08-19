d2fb14122331a11764f4a5e1040bde2bf610014e
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-08-19 14:31:05 -0500

    19 : auto detect external, phone, and email links

--- src/Link/Link.jsx
+++ src/Link/Link.jsx
@@ -2,16 +2,29 @@ import React from 'react'
 import { Icon } from '../Icon'
 
 import './Link.css'
+import classNames from 'classnames'
 
-export const Link = ({children, link, newTab = false}) => {
-  return (
-    <a 
-        className = "link"
-        href = {link} 
-        target={newTab? '_blank' : '_top'}
-    >
-        {children}
-        {newTab && <Icon name="ExternalLink" strokeWidth={1.5} size={14}></Icon>}    
-    </a>
-  )
+export const Link = ({
+    children, 
+    link, 
+    target = null,
+    title = null
+}) => {
+    const iconName = classNames({
+        'Mail': link.startsWith('mailto:'),
+        'Phone': link.startsWith('tel:'),
+        'ExternalLink' : link.startsWith('https://www.') || link.startsWith('http://www.')
+    })
+
+    return (
+        <a 
+            className = "link"
+            href = {link} 
+            target = {target}
+            title={title}
+        >
+            {children}
+            {iconName != "" && <Icon name={iconName} strokeWidth={1.5} size={14}></Icon>}    
+        </a>
+    )
 }

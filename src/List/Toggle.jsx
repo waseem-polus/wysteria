b1e5b1f39ce7864b391c2bbb703ea87db255ae9a
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-08-31 23:49:45 -0500

    37 : Added forward ref to Toggle

--- src/List/Toggle.jsx
+++ src/List/Toggle.jsx
@@ -1,59 +1,100 @@
-import React, { useState } from 'react'
+import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react'
 import { Container } from '../Container'
 import { Icon } from '../Icon'
 import classNames from 'classnames'
 
 import './Toggle.css'
 
-export const Toggle = ({ variant, children, setIsChecked, isChecked, onChange = () => {} }) => {
-    const [isHovering, setIsHovering] = useState(false)
-
-    let toggleClass = classNames(
-        'toggle',
-        `toggle--${variant}`,
+export const Toggle = forwardRef(
+    function Toggle(
         { 
-            'toggle--checked': isChecked,
-            'toggle--hover': isHovering,
-        }
-    )
+            variant, 
+            children, 
+            setIsChecked : extSetIsChecked, 
+            isChecked : extIsChecked = false, 
+            disabled = false, 
+            onChange = () => {} 
+        }, 
+        ref
+    ) {
+        const [isHovering, setIsHovering] = useState(false)
+        const [isChecked, setIsChecked] = useState(false)
 
-    let iconClass = classNames(
-        'checkmark',
-        {
-            'checkmark--hover': isHovering,
-            'checkmark--checked': isChecked
-        }
-    )
+        useEffect(() => {
+            setIsChecked(extIsChecked)
+        }, [extIsChecked])
 
-    let iconName = classNames({
-        'Check': variant === "checkbox",
-        'Dot': variant === "radio",
-        'Minus': variant === "parent-checkbox"
-    })
+        useImperativeHandle(ref, () => {
+            return {
+                toggle() {
+                    setIsChecked(!isChecked)
+                    extSetIsChecked(isChecked)
 
-  return (
-    <Container 
-        onClick = {() => {
-            setIsChecked(!isChecked)
-            onChange(isChecked)
-        }}
-        setHovering = {setIsHovering}
-        
-        paddingVer = "xs"
-        paddingHor = "md"
-        gap = "md"
-        align='center'
-        style={{cursor: "pointer"}}
-    >
-        <span className={toggleClass}>
-            <Icon 
-                className = {iconClass} 
-                name = {iconName} 
-                size = {18} 
-                strokeWidth = {2.5}
-            ></Icon>
-        </span>
-        <label className='toggle__label'>{children}</label>
-    </Container>
-  )
-}
+                    return isChecked
+                },
+                setIsChecked(value) {
+                    setIsChecked(value)
+                    extSetIsChecked(value)
+
+                    return value
+                }
+            }
+        }, [])
+
+        let toggleClass = classNames(
+            'toggle',
+            `toggle--${variant}`,
+            { 
+                'toggle--checked': !disabled && isChecked,
+                'toggle--hover': !disabled && isHovering,
+                'toggle--disabled': disabled
+            }
+        )
+
+        let iconClass = classNames(
+            'checkmark',
+            {
+                'checkmark--hover': !disabled && isHovering,
+                'checkmark--checked': !disabled && isChecked,
+            }
+        )
+
+        let labelClass = classNames(
+            'toggle__label',
+            { 'toggle__label--disabled': disabled }
+        )
+
+        let iconName = classNames({
+            'Check': variant === "checkbox",
+            'Dot': variant === "radio",
+            'Minus': variant === "parent-checkbox"
+        })
+
+        return (
+            <Container 
+                onClick = {() => {
+                    setIsChecked(!isChecked)
+                    extSetIsChecked(isChecked)
+                    onChange(isChecked)
+                }}
+                setHovering = {setIsHovering}
+                
+                paddingVer = "xs"
+                paddingHor = "md"
+                gap = "md"
+                align='center'
+                style={disabled? {cursor: "not-allowed"} : {cursor: "pointer"}}
+            >
+                <span className={toggleClass}>
+                    <Icon 
+                        className = {iconClass} 
+                        name = {iconName} 
+                        size = {18} 
+                        strokeWidth = {2.5}
+                    ></Icon>
+                </span>
+                <label className={labelClass}>{children}</label>
+            </Container>
+        )
+    }
+)

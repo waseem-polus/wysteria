a817e7568c89dc93b1569df47bf44b9b4ca115c0
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-08-31 23:50:42 -0500

    37 : Make use of native html elements

--- src/List/CheckList.jsx
+++ src/List/CheckList.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useId, useState } from 'react'
 import { Container } from '../Container'
 import { Toggle } from './Toggle'
+import { Icon } from '../Icon'
 import classNames from 'classnames'
 
+import './CheckList.css'
+
 export const CheckList = ({children, title = null}) => {
-    const [areAllChecked, setAreAllChecked] = useState(false)
     const [checkedCount, setCheckedCount] = useState(0)
-    
+
     const countChecked = () => children.filter(checkbox => checkbox.props.isChecked).length;
     useEffect(() => {
         setCheckedCount(countChecked)
@@ -20,43 +22,83 @@ export const CheckList = ({children, title = null}) => {
     })
 
     return (
-        <Container 
-            padding = "none"
-            gap = "md"
-            direction = "column"
-        >
-            {
-                title !== null && 
-                    <Toggle 
-                        setIsChecked = {setAreAllChecked} 
-                        isChecked = {checkedCount > 0}
-                        onChange={(value) => setAll(!value)}
-                        variant = {parentCheckVariant}
-                    >{title}</Toggle>
-            }
-            
+        <>
+        {
+            title !== null && 
+                <Toggle  
+                    isChecked = {checkedCount > 0}
+                    onChange={(value) => setAll(!value)}
+                    variant = {parentCheckVariant}
+                >{title}</Toggle>
+        }
             <Container 
-                paddingVer = {title !== null? "none" : "md"}
-                paddingLft = {title !== null? "xl" : "none"}
-                paddingRgt = "none"
+                padding = "none"
                 gap = "md"
                 direction = "column"
-            >
-                {children}
+            >        
+                <fieldset>
+                    {children}
+                </fieldset>
             </Container>
-        </Container>
+        </>
     )
 }
 
-export const Check = ({children, setIsChecked, isChecked, onChange = () => {}}) => {
+export const Check = ({children, checked = false, disabled = false, onChange = () => {}}) => {
+    const [isChecked, setIsChecked] = useState(checked)
+    const [isHovering, setIsHovering] = useState(false)
+    const checkboxID = useId();
+    
+    useEffect(() => {
+        setIsChecked(checked)
+    }, [checked])
+    
+    let toggleClass = classNames(
+        'toggle',
+        'toggle--checkbox',
+        { 
+            'toggle--checked': !disabled && isChecked,
+            'toggle--hover': !disabled && isHovering,
+            'toggle--disabled': disabled
+        }
+    )
+
+    let iconClass = classNames('checkmark')
+
+    let labelClass = classNames('toggle__label')
+
   return (
-    <Toggle 
-        variant="checkbox" 
-        setIsChecked={setIsChecked} 
-        isChecked={isChecked} 
-        onChange={onChange}
-    >
-        {children}
-    </Toggle>
+    <Container setHovering = {setIsHovering} padding='none'>
+        <input 
+            className = 'wui-html-checkbox' 
+            type='checkbox' 
+            id={checkboxID}
+
+            disabled = {disabled}
+
+            onChange={(e) => {
+                setIsChecked(e.target.checked)
+                onChange(e)
+            }}
+        />
+        <label htmlFor={checkboxID}>
+            <Container             
+                paddingVer = "xs"
+                paddingHor = "md"
+                gap = "md"
+                align='center'
+            >
+                <span className={toggleClass}>
+                    <Icon 
+                        className = {iconClass} 
+                        name = "Check" 
+                        size = {18} 
+                        strokeWidth = {2.5}
+                    ></Icon>
+                </span>
+                <p className={labelClass}>{children}</p>
+            </Container>
+        </label>
+    </Container>
   )
 }

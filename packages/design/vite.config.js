289a6c025d21173ed35fff40b52d64f053ffe27f
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-09-10 13:02:29 -0500

    45: Reorganized dir and removed un-used files

--- packages/design/vite.config.js
+++ packages/design/vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, './src/index.js'),
+      entry: resolve(__dirname, './index.js'),
       name: 'WUI Design',
       fileName: 'wui-design',
     },
---
+++ packages/extras/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, './index.js'),
+      name: 'WUI Extras',
+      fileName: 'wui-extras',
+    },
+    css: {
+      modules: true, // Enable CSS Modules if needed
+    }
+  }
+})

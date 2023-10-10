e3214e75dd58a3600ff72e8ebd9b045b7aa15799
waseem-polus <waseem_imad@yahoo.com>
2023-10-10 17:22:13 -0500

    85: Added useToggle hook

---
+++ packages/design/src/hooks/useToggle.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+const useToggle = ({initialValue, onChange}) => {
+	const [value, setValue] = useState(initialValue);
+
+	const toggleValue = () => setValue(!value);
+	useEffect(() => onChange(value), [value]);
+
+	return [ value, toggleValue ];
+};
+
+export default useToggle;

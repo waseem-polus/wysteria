31d7ae0b563d461b79588355d94ee86021682560
waseem-polus <waseem_imad@yahoo.com>
2024-09-14 13:50:58 -0500

    11: add code to dialog docs

--- docs/components/ComponentPreview.jsx
+++ docs/components/ComponentPreview.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ComponentPreview = ({ children }) => {
     return (
-        <div className="grid min-h-60 w-full place-items-center rounded-md border border-zinc-300 dark:border-zinc-600">
+        <div className="my-2 mt-8 grid min-h-60 w-full place-items-center rounded-md border border-zinc-300 dark:border-zinc-600">
             {children}
         </div>
     );

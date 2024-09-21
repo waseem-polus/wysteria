556a8a9f130b4cec87b0f6cc2783b9e896a0ddd4
waseem-polus <waseem_imad@yahoo.com>
2024-09-21 10:37:50 -0500

    100: Replaced old default button prop

--- packages/design/src/Button/Button.jsx
+++ packages/design/src/Button/Button.jsx
@@ -6,17 +6,11 @@ export const Button = ({
     onClick,
     children = null,
     size = "normal",
-    variant = "primary",
+    variant = "filled",
     action = "progressive",
     disabled = false,
     className = "",
 }) => {
-    const approvedVariants = ['filled', 'ghost', 'outline', 'text'];
-
-    if (!approvedVariants.includes(variant)) {
-        console.error(`used ${variant} for variant you stoopid`)
-    }
-
     return (
         <button
             className={twMerge(button({ variant, action, size }), className)}

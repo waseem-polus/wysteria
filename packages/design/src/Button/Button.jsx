905ba7dd017cb5c00c27c1860fc6735b79339f7a
waseem-polus <waseem_imad@yahoo.com>
2024-09-23 07:44:09 -0500

    100: forward toggle and button refs

--- packages/design/src/Button/Button.jsx
+++ packages/design/src/Button/Button.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
 import { button } from "./styles";
+import { forwardRef } from "react";
 
-export const Button = ({
-    onClick,
-    children = null,
-    size = "normal",
-    variant = "filled",
-    action = "progressive",
-    disabled = false,
-    className = "",
-}) => {
-    return (
-        <button
-            className={twMerge(button({ variant, action, size }), className)}
-            onClick={onClick}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    );
-};
+export const Button = forwardRef(
+    (
+        {
+            onClick,
+            children = null,
+            size = "normal",
+            variant = "filled",
+            action = "progressive",
+            disabled = false,
+            className = "",
+            ...props
+        },
+        ref,
+    ) => {
+        return (
+            <button
+                className={twMerge(
+                    button({ variant, action, size }),
+                    className,
+                )}
+                onClick={onClick}
+                disabled={disabled}
+                ref={ref}
+                {...props}
+            >
+                {children}
+            </button>
+        );
+    },
+);
--- packages/design/src/Button/Toggle.jsx
+++ packages/design/src/Button/Toggle.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import useToggle from "../hooks/useToggle";
 import { Button } from "./Button";
+import { forwardRef } from "react";
 
-export const Toggle = ({
-    children,
-    offChildren,
-    initialValue = true,
-    onChange = () => {},
-    ...props
-}) => {
-    const [value, toggleValue] = useToggle(initialValue, onChange);
+export const Toggle = forwardRef(
+    (
+        {
+            children,
+            offChildren,
+            initialValue = true,
+            onChange = () => {},
+            ...props
+        },
+        ref,
+    ) => {
+        const [value, toggleValue] = useToggle(initialValue, onChange);
 
-    return (
-        <Button onClick={() => toggleValue()} {...props}>
-            {value ? children : offChildren}
-        </Button>
-    );
-};
+        return (
+            <Button onClick={() => toggleValue()} ref={ref} {...props}>
+                {value ? children : offChildren}
+            </Button>
+        );
+    },
+);

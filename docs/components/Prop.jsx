2465c6de548d3c63a270264ee8f818da1b1fab7d
waseem-polus <waseem_imad@yahoo.com>
2024-09-24 23:01:50 -0500

    66: remove old chip prop

--- docs/components/Prop.jsx
+++ docs/components/Prop.jsx
@@ -3,12 +3,7 @@ import React from "react";
 
 const Prop = ({ children, ...props }) => {
     return (
-        <Chip
-            className="justify-center text-sm"
-            variant="outline"
-            color="primary"
-            {...props}
-        >
+        <Chip className="justify-center text-sm" variant="outline" {...props}>
             <code>{children}</code>
         </Chip>
     );

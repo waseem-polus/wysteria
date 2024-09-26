da886e7820a5195347f145f29958f036a20970e6
waseem-polus <waseem_imad@yahoo.com>
2024-09-25 23:54:56 -0500

    103: Added popover + start docs

---
+++ packages/design/src/Popover/Popover.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import * as RadixPopover from "@radix-ui/react-popover";
+import { twMerge } from "tailwind-merge";
+
+const PopoverTrigger = ({ children, asChild = true, ...props }) => {
+    return (
+        <RadixPopover.Trigger asChild={asChild} {...props}>
+            {children}
+        </RadixPopover.Trigger>
+    );
+};
+
+const PopoverAnchor = ({ children, ...props }) => {
+    return <RadixPopover.Anchor {...props}>{children}</RadixPopover.Anchor>;
+};
+
+const PopoverContent = ({ children, className = "", ...props }) => {
+    return (
+        <RadixPopover.Portal>
+            <RadixPopover.Content
+                className={twMerge(
+                    "animate-show-popover rounded-md border border-zinc-300 bg-zinc-50 p-4 dark:border-zinc-600 dark:bg-zinc-800",
+                    className,
+                )}
+                {...props}
+            >
+                {children}
+            </RadixPopover.Content>
+        </RadixPopover.Portal>
+    );
+};
+
+const PopoverClose = ({ children, asChild = true, ...props }) => {
+    return (
+        <RadixPopover.Close asChild={asChild} {...props}>
+            {children || (
+                <Button
+                    size="icon"
+                    variant="text"
+                    action="neutral"
+                    className="absolute right-3 top-3 p-0 text-zinc-500 dark:text-zinc-400"
+                >
+                    <X />
+                </Button>
+            )}
+        </RadixPopover.Close>
+    );
+};
+
+const PopoverArrow = ({ className, ...props }) => {
+    return (
+        <RadixPopover.Arrow
+            className={twMerge(
+                "border-zinc-300 fill-zinc-300 dark:border-zinc-600 dark:fill-zinc-600",
+                className,
+            )}
+            {...props}
+        />
+    );
+};
+
+const Popover = ({ children, ...props }) => {
+    return <RadixPopover.Root {...props}>{children}</RadixPopover.Root>;
+};
+
+export {
+    Popover,
+    PopoverTrigger,
+    PopoverAnchor,
+    PopoverContent,
+    PopoverClose,
+    PopoverArrow
+};
--- packages/design/tailwind.config.js
+++ packages/design/tailwind.config.js
@@ -9,10 +9,11 @@ export default {
         },
         extend: {
             animation: {
+                "show-dialog": "dialog 150ms cubic-bezier(0.16, 1, 0.3, 1)",
                 "show-popover": "popover 150ms cubic-bezier(0.16, 1, 0.3, 1)",
             },
             keyframes: {
-                popover: {
+                dialog: {
                     "0%": {
                         opacity: 0,
                         transform: "translate(-50%, -48%) scale(0.96)",
@@ -20,6 +21,16 @@ export default {
                     "100%": {
                         opacity: 1,
                         transform: "translate(-50%, -50%) scale(1)",
+                    },
+                },
+                popover: {
+                    "0%": {
+                        opacity: 0,
+                        transform: "scale(0.96)",
+                    },
+                    "100%": {
+                        opacity: 1,
+                        transform: "scale(1)",
                     },
                 },
             },

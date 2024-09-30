b3efe3bd318b8a16f086e6c5497520a72edda286
Waseem Polus <69316929+waseem-polus@users.noreply.github.com>
2024-09-29 20:16:46 -0500

    106: Toggle off/on + chip variants typo

--- packages/design/src/Button/Toggle.jsx
+++ packages/design/src/Button/Toggle.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import useToggle from "../hooks/useToggle";
 import { Button } from "./Button";
 import { forwardRef } from "react";
 
-export const Toggle = forwardRef(
-    (
-        {
-            children,
-            offChildren,
-            initialValue = true,
-            onChange = () => {},
-            ...props
-        },
-        ref,
-    ) => {
+const ToggleContext = createContext({ value: true });
+
+const ToggleOn = ({ children }) => {
+    const { value } = useContext(ToggleContext);
+    return value && children;
+};
+
+const ToggleOff = ({ children }) => {
+    const { value } = useContext(ToggleContext);
+    return !value && children;
+};
+
+const Toggle = forwardRef(
+    ({ children, initialValue = true, onChange = () => {}, ...props }, ref) => {
         const [value, toggleValue] = useToggle(initialValue, onChange);
 
         return (
-            <Button onClick={() => toggleValue()} ref={ref} {...props}>
-                {value ? children : offChildren}
-            </Button>
+            <ToggleContext.Provider value={{ value }}>
+                <Button onClick={() => toggleValue()} ref={ref} {...props}>
+                    {children}
+                </Button>
+            </ToggleContext.Provider>
         );
     },
 );
+Toggle.displayName = "Toggle";
+
+export { Toggle, ToggleOn, ToggleOff };

02f95e9f6bc07bc6cb2648c65243fe0b2d027415
Waseem Polus <69316929+waseem-polus@users.noreply.github.com>
2024-10-01 20:32:12 -0500

    108: Add prop hints

--- docs/components/Prop.jsx
+++ docs/components/Prop.jsx
@@ -1,12 +1,40 @@
-import { Chip } from "@wysteria/design";
+import {
+	Chip,
+	ChipAction,
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@wysteria/design";
+import { Info } from "lucide-react";
 import React from "react";
+import hint from "../helpers/hint";
 
-const Prop = ({ children, ...props }) => {
-    return (
-        <Chip className="justify-center text-sm" variant="outline" {...props}>
-            <code>{children}</code>
-        </Chip>
-    );
+const Prop = ({ children, hint: hintKey = null, ...props }) => {
+	return (
+		<Chip
+			className="flex flex-grow justify-between text-sm"
+			variant="outline"
+			{...props}
+		>
+			<code className="select-text">{children}</code>
+			{hint !== "" && (
+				<Popover>
+					<PopoverTrigger>
+						<ChipAction>
+							<Info size={16} />
+						</ChipAction>
+					</PopoverTrigger>
+					<PopoverContent
+						sideOffset={10}
+						side="bottom"
+						className="max-w-[48ch] text-sm"
+					>
+						{hint(hintKey ?? children)}
+					</PopoverContent>
+				</Popover>
+			)}
+		</Chip>
+	);
 };
 
 export default Prop;
---
+++ docs/helpers/hint.js
@@ -0,0 +1,7 @@
+import PROP_HINTS from "./propHints";
+
+const hint = (key) => {
+	return PROP_HINTS[key];
+};
+
+export default hint;

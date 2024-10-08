11c3b22c770f65119661dd30e56453ff84638c00
waseem-polus <waseem_imad@yahoo.com>
2024-10-08 12:03:34 -0500

    108: merge changes from dev

--- docs/pages/_app.jsx
+++ docs/pages/_app.jsx
@@ -1,6 +1,6 @@
+import React from "react";
 import "../public/index.css";
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
-
--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
-		"./components/**/*.{js,jsx,ts,tsx,md,mdx}",
-	],
-	darkMode: "selector",
-	important: "#__next",
+    content: [
+        "./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
+        "./components/**/*.{js,jsx,ts,tsx,md,mdx}",
+    ],
+    darkMode: "selector",
+    important: "#__next",
 };

b0c36c7562d837c16da7023fcc36b486052561fb
Waseem Polus <69316929+waseem-polus@users.noreply.github.com>
2025-03-22 10:17:50 -0500

    96: Update documentation (#111)
    * 96: Start working on Getting Started page
    
    * 63: Basic input
    
    * 63: Add search action
    
    * add tel to docs
    
    * 63: remove old inputs from doc page
    
    * 63: Add url and email inputs to docs
    
    * 63: Add twMerge to input classname
    
    * 63: Added input icon
    
    * 63: Add disabled styling
    
    * 96: Add login dialog example to getting started
    
    * 96: fix import statement
    
    * 96: empty line format for getting started code example
    
    * 112: Refine links and update their docs
    
    * 112: rtremove quotes around <a>
    
    * 63: Added input icon
    
    * 63: adjust input button sizes
    
    * 63: Add icons to inputs in docs
    
    * 96: fix deployement
    
    * 96: Add new component cards to greenhouse
    
    * 96: style greenhouse cards for smaller screens
    
    * 96: Overhaul greenhouse page
    
    * 96: Add color to pr and issue icons
    
    * 96: Remove docs pages with "coming soon"
    
    * 96: Remove edit and feedback links from docs and tweak readme

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -5,13 +5,23 @@ import { Container } from "@wysteria/design";
 export default {
     logo: (
         <Container padding="none" gap="sm" align="center">
-            <Image src="/wysteria.svg" width={40} height={40} alt="wysteria ui logo"/>
+            <Image
+                src="/wysteria.svg"
+                width={40}
+                height={40}
+                alt="wysteria ui logo"
+            />
             <h1>Wysteria UI</h1>
         </Container>
     ),
     project: {
-        link: "https://github.com/waseem-polus/wui-design"
+        link: "https://github.com/waseem-polus/wui-design",
     },
+    feedback: {
+        content: "",
+        labels: "",
+    },
+    editLink: "",
     primaryHue: {
         dark: 264,
         light: 264,
@@ -23,10 +33,16 @@ export default {
     },
     head: (
         <>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
             <link rel="icon" type="image/x-icon" href="/wysteria.svg"></link>
             <meta property="og:title" content="Wysteria UI" />
-            <meta property="og:description" content="We design, so you can create! Wysteria UI provides a collection of reusable React components to help streamline the development of web applications." />
+            <meta
+                property="og:description"
+                content="We design, so you can create! Wysteria UI provides a collection of reusable React components to help streamline the development of web applications."
+            />
         </>
-    )
+    ),
 };
